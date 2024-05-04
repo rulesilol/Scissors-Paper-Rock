a523032bc19dff835ef028bc494c450d975b4b9e
rulesilol <andrewlei323@gmail.com>
2024-05-04 22:55:50 +1000

    Added js scoring elements

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+const scissors_btn = document.querySelector(".scissors-btn")
+const paper_btn = document.querySelector(".paper-btn")
+const rock_btn = document.querySelector(".rock-btn")
+const btn = document.querySelectorAll("button")
+const scoreboard = document.querySelector(".scoreboard")
+
+let computerScore = 0
+let humanScore = 0
+
+scoreboard.innerHTML = "Score Computer: " + String(computerScore) + " Human: " + String(humanScore)
+
+scissors_btn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const winner = returnWinner("scissors", computerChoice)
+
+    if (winner == "human") {
+        humanScore += 1
+    } else if (winner == "computer") {
+        computerScore += 1
+    }
+    scoringFunction()
+})
+
+paper_btn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const winner = returnWinner("paper", computerChoice)
+
+    if (winner == "human") {
+        humanScore += 1
+    } else if (winner == "computer") {
+        computerScore += 1
+    }
+    scoringFunction()
+})
+
+rock_btn.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const winner = returnWinner("rock", computerChoice)
+
+    if (winner == "human") {
+        humanScore += 1
+    } else if (winner == "computer") {
+        computerScore += 1
+    }
+    scoringFunction()
+})
+
 function getComputerChoice() {
     function getRandomInt() {
         return Math.floor(Math.random() * 3)
@@ -12,32 +59,32 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(){
-    const humanChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase()
-    return humanChoice
+function scoringFunction() {
+    if (computerScore == 5) {
+        scoreboard.innerHTML = "Computer Wins!!!"
+        humanScore = 0
+        computerScore = 0
+    } else if (humanScore == 5) {
+        scoreboard.innerHTML = "Human Wins!!!"
+        humanScore = 0
+        computerScore = 0
+    } else {
+        scoreboard.innerHTML = "Score Computer: " + String(computerScore) + " Human: " + String(humanScore)
+    }
 }
 
-function playRounds(rounds) {
-    let humanScore = 0
-    let computerScore = 0
-    for (let i = 1; i < rounds; i++) {
-        let computerChoice = getComputerChoice()
-        let playerChoice = getHumanChoice()
-
-        if ((computerChoice == "rock" ) && (playerChoice == "paper")) {
-            humanScore += 1
-        } else if ((computerChoice == "rock" ) && (playerChoice == "scissors")) {
-            computerChoice += 1
-        } else if ((computerChoice == "paper" ) && (playerChoice == "scissors")) {
-            humanScore += 1
-        } else if ((computerChoice == "paper" ) && (playerChoice == "rock")) {
-            computerScore += 1
-        } else if ((computerChoice == "scissors" ) && (playerChoice == "rock")) {
-            humanScore += 1
-        } else if ((computerChoice == "scissors" ) && (playerChoice == "paper")) {
-            computerScore += 1
-        }
-        console.log("Human Score: ", humanScore, "Computer Score: ", computerScore)
-
+function returnWinner(playerChoice, computerChoice) {
+    if ((computerChoice == "rock" ) && (playerChoice == "paper")) {
+        return "human"
+    } else if ((computerChoice == "rock" ) && (playerChoice == "scissors")) {
+        return "computer"
+    } else if ((computerChoice == "paper" ) && (playerChoice == "scissors")) {
+        return "human"
+    } else if ((computerChoice == "paper" ) && (playerChoice == "rock")) {
+        return "computer"
+    } else if ((computerChoice == "scissors" ) && (playerChoice == "rock")) {
+        return "human"
+    } else if ((computerChoice == "scissors" ) && (playerChoice == "paper")) {
+        return "computer"
     }
 }
